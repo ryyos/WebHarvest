b5985128225d32e6dfca38dec1892937e7eb4fe9
ryosyntaxx <riodwi12174@gmail.com>
2024-03-18 15:04:32 +0700

    build: add settings

---
+++ settings.py
@@ -0,0 +1,20 @@
+S3_CONFIGURATIONS = {
+    "service_name": "s3",
+    "aws_access_key_id": "xxxxxxxxxxxxxx",
+    "aws_secret_access_key": "xxxxxxxxxxxxxx",
+    "endpoint_url": "xxxxxxxxxxxxxx",
+    "bucket": "xxxxxxxxxxxxxx",
+    "base_path_s3": 'xxxxxxxxxxxxxx'
+}
+
+BOTO3_CONFIGURATIONS = {
+    "max_attempts": 10,
+    "mode": "standard"
+}
+
+KAFKA_CONFIGURATIONS = {
+    "bootstrap_servers_1": "xxxxxxxxxxxxxx",
+    "bootstrap_servers_2": "xxxxxxxxxxxxxx",
+    "bootstrap_servers_3": "xxxxxxxxxxxxxx",
+    "topic": "xxxxxxxxxxxxxx"
+}
